1069ae83a9a073edc678321aa187fb4fa7d3d8bd
elimlk <48507321+elimlk@users.noreply.github.com>
2021-04-06 21:44:09 +0300

    Basic Music PLayer with one song only by hardcoded path

--- app/src/main/java/com/example/androidmusicplayer/MainActivity.java
+++ app/src/main/java/com/example/androidmusicplayer/MainActivity.java
@@ -40,5 +40,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void stopMusic() {
+        Intent intent = new Intent(this,MediaService.class);
+        stopService(intent);
+
     }
 }
